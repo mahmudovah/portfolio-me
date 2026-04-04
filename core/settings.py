c28712e3a90a106af241cc11bf77b7d11724e6ba
mahmudovah <mahmudovahadija.0831@gmail.com>
2026-04-04 14:21:53 +0500

    tuzatish

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1w1mx_%w^@tv4a_0l$9+fi490#a2pt#03&zicufcm2xkovkie!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
